1ba80b7fa672e578338466e0db546a545b21a2b4
Robin Vobruba <robin.vobruba@gmail.com>
2013-04-23 08:43:21 +0200

    Ex01: auto-download input image

--- ex01/run.sh
+++ ex01/run.sh
@@ -11,9 +11,16 @@ if [ ! -f "${EXECUTABLE}" ]; then
 	./build.sh
 fi
 
+if [ ! -f "${RESOURCES_DIR}/img/input.jpg" ]; then
+	mkdir -p "${RESOURCES_DIR}/img"
+	cd "${RESOURCES_DIR}/img/"
+	wget "http://images.paraorkut.com/img/wallpapers/1024x768/t/tiger_rabbit-7532.jpg"
+	mv tiger*.jpg input.jpg
+fi
+
 cd "${TARGET_DIR}"
 
-ARGUMENTS="${RESOURCES_DIR}/img/orig.jpg"
+ARGUMENTS="${RESOURCES_DIR}/img/input.jpg"
 
 if [ "${2}" = "debug" ]; then
 	echo "ARGUMENTS: ${ARGUMENTS}"
