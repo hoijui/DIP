83ef240ccf0253c05b606b8d8ba06d87216460cf
hoijui <hoijui.quaero@gmail.com>
2013-05-09 11:42:10 +0200

    ex02: unimportant change in run script [nlc]

--- ex02/run.sh
+++ ex02/run.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 TARGET_DIR="${SCRIPT_DIR}/target"
 RESOURCES_DIR="${SCRIPT_DIR}/src/main/resources"
 EXECUTABLE=$(ls ${TARGET_DIR}/dip2)
+SRC_IMG_URL=http://www.lolhome.com/img_big/funny-picture-9979524311.jpg
+#SRC_IMG_URL=http://www.thewallpapers.org/wallpapers/14/1420/thumb/600_los_angeles_skyline-001.jpg
 
 cd "${SCRIPT_DIR}"
 
@@ -14,7 +16,7 @@ fi
 if [ ! -f "${RESOURCES_DIR}/img/input.jpg" ]; then
 	mkdir -p "${RESOURCES_DIR}/img"
 	cd "${RESOURCES_DIR}/img/"
-	wget "http://www.lolhome.com/img_big/funny-picture-9979524311.jpg"
+	wget "${SRC_IMG_URL}"
 	mv *.jpg input.jpg
 fi
 
