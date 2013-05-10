9f0d92e2cd69ac0ea8916228f946643431f9a4d7
Robin Vobruba <robin.vobruba@gmail.com>
2013-05-10 10:34:34 +0200

    ex03: initial commit

---
+++ ex03/run.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+TARGET_DIR="${SCRIPT_DIR}/target"
+RESOURCES_DIR="${SCRIPT_DIR}/src/main/resources"
+EXECUTABLE=$(ls ${TARGET_DIR}/dip3)
+SRC_IMG_URL=http://www.lolhome.com/img_big/funny-picture-9979524311.jpg
+#SRC_IMG_URL=http://www.thewallpapers.org/wallpapers/14/1420/thumb/600_los_angeles_skyline-001.jpg
+
+cd "${SCRIPT_DIR}"
+
+if [ ! -f "${EXECUTABLE}" ]; then
+	./build.sh
+fi
+
+if [ ! -f "${RESOURCES_DIR}/img/input.jpg" ]; then
+	mkdir -p "${RESOURCES_DIR}/img"
+	cd "${RESOURCES_DIR}/img/"
+	wget "${SRC_IMG_URL}"
+	mv *.jpg input.jpg
+fi
+
+cd "${TARGET_DIR}"
+
+ARGUMENTS="generate ${RESOURCES_DIR}/img/input.jpg"
+
+# generate noisy images
+if [ ! -f "original.jpg" ]; then
+	"${EXECUTABLE}" ${ARGUMENTS}
+fi
+
+ARGUMENTS="restorate"
+
+if [ "${2}" = "debug" ]; then
+	echo "ARGUMENTS: ${ARGUMENTS}"
+	ddd "${EXECUTABLE}" &
+else
+	"${EXECUTABLE}" ${ARGUMENTS}
+fi
+
