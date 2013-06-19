95d285251ec6c4c9f8a7443a6ce0a199e5e0d060
Robin Vobruba <robin.vobruba@gmail.com>
2013-06-19 21:49:26 +0200

    TMP temporary inverse filter code (about half of it)

--- ex04/src/main/native/dip4.cpp
+++ ex04/src/main/native/dip4.cpp
@@ -16,7 +16,45 @@
 
 Mat inverseFilter(Mat& degraded, Mat& filter) {
 
-	// TODO
+	const float epsilon = 0.05f;
+
+	const Mat& s = degraded;
+	const Mat& p = filter;
+	const int kw = p.cols;
+	const int kh = p.rows;
+
+	// transform into frequency domain
+	Mat S(s.size(), CV_32FC2); // complex
+	Mat P(p.size(), CV_32FC2); // complex
+	dft(s, S);
+	dft(p, P);
+
+	// create the inverse filter
+	// see DIP04_ST13_wiener.pdf page 12
+	const float maxAbsP = max(abs(P));
+	const float epsMaxAbsP = epsilon * maxAbsP;
+
+	// TODO FIXME
+	Map Q(P.size(), CV_32FC2);
+	threshold(P, Q, epsMaxAbsP, epsMaxAbsP, TYPE);
+
+	Mat QBig = Mat::zeros(S.size(), S.type());
+	for (int x = 0; x < kw; ++x) {
+		for (int y = 0; y < kh; ++y) {
+			QBig.at<float>(x, y) = Q.at<float>(x, y);
+		}
+	}
+	Mat QBigShifted = circShift(QBig, -(kw/2), -(kh/2));
+
+	// multiply with the inverse model of the distortion
+	// (== convolute in spacial domain)
+	Mat O_approx = S * QBigShifted;
+
+	// transform back into spatial domain
+	Mat o_approx(O_approx.size(), CV_32FC1);
+	dft(O_approx, o_approx, DFT_INVERSE | DFT_SCALE);
+
+	return o_approx;
 }
 
 Mat wienerFilter(Mat& degraded, Mat& filter, double snr) {
