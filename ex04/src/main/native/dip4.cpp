15aa915f4dd8deadae3089a37f6612238bbe485c
hoijui <hoijui.quaero@gmail.com>
2013-06-20 11:13:49 +0200

    TMP trying a lot of stuff for ex04 inverse! (friggn complex!)

--- ex04/src/main/native/dip4.cpp
+++ ex04/src/main/native/dip4.cpp
@@ -23,36 +23,120 @@ Mat inverseFilter(Mat& degraded, Mat& filter) {
 	const int kw = p.cols;
 	const int kh = p.rows;
 
+	Mat pBig = Mat::zeros(s.size(), s.type());
+	for (int x = 0; x < kw; ++x) {
+		for (int y = 0; y < kh; ++y) {
+			pBig.at<float>(x, y) = p.at<float>(x, y);
+		}
+	}
+	Mat pBigShifted(s.size(), s.type());
+	circShift(pBig, pBigShifted, -(kw/2), -(kh/2));
+
 	// transform into frequency domain
 	Mat S(s.size(), CV_32FC2); // complex
-	Mat P(p.size(), CV_32FC2); // complex
-	dft(s, S);
-	dft(p, P);
+	Mat P(pBigShifted.size(), CV_32FC2); // complex
+	dft(s, S, DFT_COMPLEX_OUTPUT);
+	dft(pBigShifted, P, DFT_COMPLEX_OUTPUT);
+//	dft(s, S);
+//	dft(pBigShifted, P);
+
+
+	const int w = src.rows;
+	const int h = src.cols;
+	const int kw = kernel.rows;
+	const int kh = kernel.cols;
+	const int kwHalf = (int) (kw / 2);
+	const int khHalf = (int) (kh / 2);
+	for (int x = 0; x < w; ++x) { // image.x
+		for (int y = 0; y < h; ++y) { // image.y
+			float newVal = 0.0f;
+			for (int kx = 0; kx < kw; ++kx) { // kernel.x
+				for (int ky = 0; ky < kh; ++ky) { // kernel.y
+					int imgX = x - kwHalf + kx;
+					cropCoordinate(imgX, 0, w);
+					int imgY = y - khHalf + ky;
+					cropCoordinate(imgY, 0, h);
+					newVal += src.at<float>(imgX, imgY) * kernel.at<float>(kx, ky);
+				}
+			}
+			res.at<float>(x, y) = newVal;
+		}
+	}
+
+
 
 	// create the inverse filter
 	// see DIP04_ST13_wiener.pdf page 12
-	const float maxAbsP = max(abs(P));
+	Mat PAbs = abs(P);
+//	const float maxAbsP = max(PAbs);
+	double maxAbsP;
+//	int maxIdx[3];
+	minMaxIdx(PAbs, 0, &maxAbsP, 0, 0);
 	const float epsMaxAbsP = epsilon * maxAbsP;
 
 	// TODO FIXME
-	Map Q(P.size(), CV_32FC2);
-	threshold(P, Q, epsMaxAbsP, epsMaxAbsP, TYPE);
+	Mat Q(PAbs.size(), CV_32FC2);
+	Mat Preciproce(P.size(), CV_32FC2);
+	Mat PIsBig(PAbs.size(), CV_32FC1);
+	Mat PIsSmall(PAbs.size(), CV_32FC1);
+	Mat Pthresholded(PAbs.size(), CV_32FC1);
+	Mat PthresholdedComp(P.size(), CV_32FC2);
+	threshold(PAbs, PIsBig, epsMaxAbsP, 1.0, THRESH_BINARY);
+	threshold(PAbs, PIsSmall, epsMaxAbsP, 1.0, THRESH_BINARY_INV);
 
-	Mat QBig = Mat::zeros(S.size(), S.type());
-	for (int x = 0; x < kw; ++x) {
-		for (int y = 0; y < kh; ++y) {
-			QBig.at<float>(x, y) = Q.at<float>(x, y);
-		}
+//	vector<Mat> complexChannelsBig(2);
+//	split(PIsBig, complexChannelsBig);
+////	PIsBig = complexChannelsBig.at(0);
+//	complexChannelsBig.at(1) = complexChannelsBig.at(0);
+//	merge(complexChannelsBig, PIsBig);
+
+//	vector<Mat> complexChannelsSmall(2);
+//	split(PIsSmall, complexChannelsSmall);
+////	PIsSmall = complexChannelsSmall.at(0);
+//	complexChannelsSmall.at(1) = complexChannelsSmall.at(0);
+//	merge(complexChannelsSmall, PIsSmall);
+
+//	Q = ((1 / P) * PIsBig) + (epsMaxAbsP * PIsSmall);
+//	Q = 1.0 / P;
+//	Q *= PIsBig;
+		divide(1.0, P, Preciproce);
+		multiply(Preciproce, PIsBig, Q);
+		multiply(epsMaxAbsP, PIsSmall, Pthresholded);
+//		vector<Mat> toBeMerged;
+//		toBeMerged.push_back(Pthresholded);
+//		toBeMerged.push_back(Mat::zeros(P.size(), CV_32FC1));
+//		merge(toBeMerged, PthresholdedComp);
+		PthresholdedComp = Pthresholded;
+//		cout << "PIsSmall: " << PIsSmall << endl;
+		cout << "CV_32FC1: " << CV_32FC1 << endl;
+		cout << "CV_32FC2: " << CV_32FC2 << endl;
+		cout << "PIsSmall: " << PIsSmall.type() << endl;
+		cout << "Pthresholded: " << Pthresholded.type() << endl;
+		cout << "Mat::zeros(P.size(), CV_32FC1): " << Mat::zeros(P.size(), CV_32FC1).type() << endl;
+		cout << "Q.size: " << Q.size[0] << "*" << Q.size[1] << " type:" << Q.type() << endl;
+		cout << "PthresholdedComp.size: " << PthresholdedComp.size[0] << "*" << PthresholdedComp.size[1] << " type:" << PthresholdedComp.type() << endl;
+		Q += PthresholdedComp;
+	try {
+	} catch (const exception& ex) {
+		cout << "ERROR: " << ex.what() << endl;
 	}
-	Mat QBigShifted = circShift(QBig, -(kw/2), -(kh/2));
+
+//	Q = ((1 / P) * PIsBig);
+	Q += (epsMaxAbsP * PIsSmall);
 
 	// multiply with the inverse model of the distortion
 	// (== convolute in spacial domain)
-	Mat O_approx = S * QBigShifted;
+//	Mat O_approx = S * Q;
+	Mat O_approx(P.size(), P.type());
+	multiply(S, Q, O_approx);
 
 	// transform back into spatial domain
 	Mat o_approx(O_approx.size(), CV_32FC1);
-	dft(O_approx, o_approx, DFT_INVERSE | DFT_SCALE);
+//	dft(O_approx, o_approx, DFT_INVERSE | DFT_SCALE);
+	dft(O_approx, o_approx, DFT_INVERSE| DFT_SCALE | DFT_REAL_OUTPUT);
+//	dft(O_approx, o_approx, DFT_INVERSE | DFT_SCALE | DFT_COMPLEX_OUTPUT);
+	cout << "O_approx: " << O_approx.type() << endl;
+	cout << "o_approx: " << o_approx.type() << endl;
 
 	return o_approx;
 }
