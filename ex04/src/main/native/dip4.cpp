644f55840796ea50f2665196d03516ad2807ff89
hoijui <hoijui.quaero@gmail.com>
2013-06-21 07:48:16 +0200

    ex04: buggy version of inverse filter

--- ex04/src/main/native/dip4.cpp
+++ ex04/src/main/native/dip4.cpp
@@ -37,96 +37,51 @@ Mat inverseFilter(Mat& degraded, Mat& filter) {
 	Mat P(pBigShifted.size(), CV_32FC2); // complex
 	dft(s, S, DFT_COMPLEX_OUTPUT);
 	dft(pBigShifted, P, DFT_COMPLEX_OUTPUT);
-//	dft(s, S);
-//	dft(pBigShifted, P);
-
 
 	const int w = P.rows;
 	const int h = P.cols;
-//	float PiAbsMax = 0.0f;
-	for (int x = 0; x < w; ++x) { // image.x
-		for (int y = 0; y < h; ++y) { // image.y
-			float* Pi = P.at<float*>(x, y);
+	float PiAbsMax = 0.0f;
+	Mat Pabs(P.size(), CV_32FC1); // abs(P)
+	for (int x = 0; x < w; ++x) {
+		for (int y = 0; y < h; ++y) {
+			Vec2f Pi = P.at<Vec2f>(x, y);
 			float PiAbs = sqrt((Pi[0] * Pi[0]) + (Pi[1] * Pi[1]));
+			if (PiAbs > PiAbsMax) {
+				PiAbsMax = PiAbs;
+			}
 			Pabs.at<float>(x, y) = PiAbs;
 		}
 	}
-	float PiAbsMax = max(PiAbsMax);
-
-
 
 	// create the inverse filter
 	// see DIP04_ST13_wiener.pdf page 12
-	Mat PAbs = abs(P);
-//	const float maxAbsP = max(PAbs);
-	double maxAbsP;
-//	int maxIdx[3];
-	minMaxIdx(PAbs, 0, &maxAbsP, 0, 0);
-	const float epsMaxAbsP = epsilon * maxAbsP;
+	const float epsMaxAbsP = epsilon * PiAbsMax;
 
-	// TODO FIXME
-	Mat Q(PAbs.size(), CV_32FC2);
+	Mat Q(Pabs.size(), CV_32FC2);
 	Mat Preciproce(P.size(), CV_32FC2);
-	Mat PIsBig(PAbs.size(), CV_32FC1);
-	Mat PIsSmall(PAbs.size(), CV_32FC1);
-	Mat Pthresholded(PAbs.size(), CV_32FC1);
-	Mat PthresholdedComp(P.size(), CV_32FC2);
-	threshold(PAbs, PIsBig, epsMaxAbsP, 1.0, THRESH_BINARY);
-	threshold(PAbs, PIsSmall, epsMaxAbsP, 1.0, THRESH_BINARY_INV);
+	divide(1.0, P, Preciproce);
 
-//	vector<Mat> complexChannelsBig(2);
-//	split(PIsBig, complexChannelsBig);
-////	PIsBig = complexChannelsBig.at(0);
-//	complexChannelsBig.at(1) = complexChannelsBig.at(0);
-//	merge(complexChannelsBig, PIsBig);
-
-//	vector<Mat> complexChannelsSmall(2);
-//	split(PIsSmall, complexChannelsSmall);
-////	PIsSmall = complexChannelsSmall.at(0);
-//	complexChannelsSmall.at(1) = complexChannelsSmall.at(0);
-//	merge(complexChannelsSmall, PIsSmall);
-
-//	Q = ((1 / P) * PIsBig) + (epsMaxAbsP * PIsSmall);
-//	Q = 1.0 / P;
-//	Q *= PIsBig;
-		divide(1.0, P, Preciproce);
-		multiply(Preciproce, PIsBig, Q);
-		multiply(epsMaxAbsP, PIsSmall, Pthresholded);
-//		vector<Mat> toBeMerged;
-//		toBeMerged.push_back(Pthresholded);
-//		toBeMerged.push_back(Mat::zeros(P.size(), CV_32FC1));
-//		merge(toBeMerged, PthresholdedComp);
-		PthresholdedComp = Pthresholded;
-//		cout << "PIsSmall: " << PIsSmall << endl;
-		cout << "CV_32FC1: " << CV_32FC1 << endl;
-		cout << "CV_32FC2: " << CV_32FC2 << endl;
-		cout << "PIsSmall: " << PIsSmall.type() << endl;
-		cout << "Pthresholded: " << Pthresholded.type() << endl;
-		cout << "Mat::zeros(P.size(), CV_32FC1): " << Mat::zeros(P.size(), CV_32FC1).type() << endl;
-		cout << "Q.size: " << Q.size[0] << "*" << Q.size[1] << " type:" << Q.type() << endl;
-		cout << "PthresholdedComp.size: " << PthresholdedComp.size[0] << "*" << PthresholdedComp.size[1] << " type:" << PthresholdedComp.type() << endl;
-		Q += PthresholdedComp;
-	try {
-	} catch (const exception& ex) {
-		cout << "ERROR: " << ex.what() << endl;
+	for (int x = 0; x < w; ++x) {
+		for (int y = 0; y < h; ++y) {
+			const float PiAbs = Pabs.at<float>(x, y);
+			Vec2f Qi(0.0f, 0.0f);
+			if (PiAbs >= epsMaxAbsP) {
+				Qi = Preciproce.at<Vec2f>(x, y);
+			} else {
+				Qi[0] = epsMaxAbsP;
+			}
+			Q.at<Vec2f>(x, y) = Qi;
+		}
 	}
-
-//	Q = ((1 / P) * PIsBig);
-	Q += (epsMaxAbsP * PIsSmall);
 
 	// multiply with the inverse model of the distortion
 	// (== convolute in spacial domain)
-//	Mat O_approx = S * Q;
 	Mat O_approx(P.size(), P.type());
 	multiply(S, Q, O_approx);
 
 	// transform back into spatial domain
 	Mat o_approx(O_approx.size(), CV_32FC1);
-//	dft(O_approx, o_approx, DFT_INVERSE | DFT_SCALE);
 	dft(O_approx, o_approx, DFT_INVERSE| DFT_SCALE | DFT_REAL_OUTPUT);
-//	dft(O_approx, o_approx, DFT_INVERSE | DFT_SCALE | DFT_COMPLEX_OUTPUT);
-	cout << "O_approx: " << O_approx.type() << endl;
-	cout << "o_approx: " << o_approx.type() << endl;
 
 	return o_approx;
 }
@@ -134,6 +89,7 @@ Mat inverseFilter(Mat& degraded, Mat& filter) {
 Mat wienerFilter(Mat& degraded, Mat& filter, double snr) {
 
 	// TODO
+	return degraded;
 }
 
 /*
