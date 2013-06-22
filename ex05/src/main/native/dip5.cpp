216b91ce4fc8f82db21918a8ec44317f66e7a869
hoijui <hoijui.quaero@gmail.com>
2013-06-22 17:23:53 +0200

    ex05: start implementing TMP

--- ex05/src/main/native/dip5.cpp
+++ ex05/src/main/native/dip5.cpp
@@ -7,17 +7,34 @@
 //               defines and plots interest points
 // =============================================================================
 
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
 #include "dip5.hpp"
 
-void getInterestPoints(Mat& img, double sigma, vector<KeyPoint>& points){
+void getInterestPoints(Mat& img, double sigma, vector<KeyPoint>& points) {
 
 	// TODO
 }
 
-void createFstDevKernel(Mat& kernel, double sigma){
+void createFstDevKernel(Mat& kernel, double sigma) {
 
-	// TODO
+	const int w = kernel.rows;
+	const int h = kernel.cols;
+	const float sigma22 = 2 * sigma * sigma;
+	const float sigma4Pi = 2 * M_PI * sigma * sigma * sigma * sigma;
+	const float hw = (w / 2) + 0.5f;
+	const float hh = (h / 2) + 0.5f;
+	for (int x = 0; x < w; ++x) {
+		const float xv = x - hw;
+		for (int y = 0; y < h; ++y) {
+			const float yv = y - hh;
+			const float commonFac = exp((xv*xv + yv*yv) / sigma22) / sigma4Pi;
+			// see DIP05_ST13_interest.pdf page 10
+			kernel.at<Vec2f>(x, y)[0] = -xv * commonFac; // partial derivative towards x
+			kernel.at<Vec2f>(x, y)[1] = -yv * commonFac; // partial derivative towards y
+		}
+	}
 }
