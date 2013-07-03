24a3c91c8d7f819d6276f193a5e95d67bd780d05
Robin Vobruba <robin.vobruba@gmail.com>
2013-07-03 07:09:34 +0200

    ex04: combine inverse and wiener filter code

--- ex04/src/main/native/dip4.cpp
+++ ex04/src/main/native/dip4.cpp
@@ -13,149 +13,130 @@
 // function headers of not yet implemented functions
 #include "dip4.hpp"
 
+static void circShiftXXX(Mat& in, Mat& out, int dx, int dy){
+
+	int dstI, dstJ;
+
+	for(int i=0; i<in.rows;i++)
+	{
+		dstI = i+dx;
+		if(dstI<0)
+		{
+			dstI += out.rows;
+		} else if(dstI>=out.rows){
+			dstI -= out.rows;
+		}
+
+		for(int j=0; j<in.cols;j++)
+		{
+			dstJ = j+dy;
+			if(dstJ<0)
+			{
+				dstJ += out.cols;
+			} else if(dstJ>=out.cols){
+				dstJ -= out.cols;
+			}
+
+			out.at<float>(dstI,dstJ) = in.at<float>(i,j);
+		}
+	}
+}
+
+static Mat inverseAndWiener(Mat& s, Mat& p, double snr, bool inverse) {
+
+	const bool wiener = !inverse;
+
+	// Pad input image to avoid ringing artifacts along image borders.
+	int bH = p.cols;
+	int bV = p.rows;
+	Mat sBorder;
+	copyMakeBorder(s, sBorder, bV, bV, bH, bH, BORDER_REPLICATE);
+
+	// Allocate some memory like it is going out of style.
+	Mat pBigShifted = Mat::zeros(sBorder.size(), CV_32F);
+	Mat P = Mat::zeros(sBorder.size(), CV_32F);
+	Mat S = Mat::zeros(sBorder.size(), CV_32F);
+	Mat OApprox = Mat::zeros(sBorder.size(), CV_32F);
+	Mat oApprox = Mat::zeros(sBorder.size(), CV_32F);
+
+	// Shift kernel.
+	const int pHalf = p.rows / 2;
+	circShiftXXX(p, pBigShifted, -pHalf, -pHalf);
+
+	// Transform shifted kernel and degrated input image into frequency domain.
+	// Note: DFT_COMPLEX_OUTPUT means that we want the complex result to be stored
+	//       in a two-channel matrix as opposed to the default compressed output.
+	dft(pBigShifted, P, DFT_COMPLEX_OUTPUT);
+	dft(sBorder, S, DFT_COMPLEX_OUTPUT);
+
+	if (inverse) {
+		const double epsilon = 0.05f;
+
+		// Remove frequencies whose magnitude is below epsilon * max(freqKernel magnitude).
+		double maxMagnitude;
+		minMaxLoc(abs(P), 0, &maxMagnitude);
+
+		const double threshold =  maxMagnitude * epsilon;
+		for (int ri = 0; ri < P.rows; ri++) {
+			for (int ci = 0; ci < P.cols; ci++) {
+				if (norm(P.at<Vec2f>(ri, ci)) < threshold) {
+					P.at<Vec2f>(ri, ci) = threshold;
+				}
+			}
+		}
+	}
+
+	// OpenCV only provides a multiplication operation for complex matrices, so we need
+	// to calculate the inverse (1/H) of our filter spectrum first. Since it is complex
+	// we need to compute 1/H = H*/(HH*) = H*/(Re(H)^2+Im(H)^2), where H* -> complex conjugate of H.
+
+	// Multiply spectrum of the degrated image with the complex conjugate of the frequency spectrum
+	// of the filter.
+	const bool conjFreqKernel = true;
+	mulSpectrums(S, P, OApprox, DFT_COMPLEX_OUTPUT, conjFreqKernel); // I * H*
+
+	// Split kernel spectrum into real and imaginary parts.
+	Mat PChannels[] = {Mat::zeros(sBorder.size(), CV_32F), Mat::zeros(sBorder.size(), CV_32F)};
+	split(P, PChannels); // 0:real, 1:imaginary
+
+	// Calculate squared magnitude (Re(H)^2 + Im(H)^2) of filter spectrum.
+	Mat freqKernelSqMagnitude = Mat::zeros(sBorder.rows, sBorder.cols, CV_32F);
+	magnitude(PChannels[0], PChannels[1], freqKernelSqMagnitude); // freqKernelSqMagnitude = magnitude
+	pow(PChannels[0], 2, freqKernelSqMagnitude); // freqKernelSqMagnitude = magnitude^2 = Re(H)^2 + Im(H)^2
+
+	if (wiener) {
+		// Add 1 / SNR^2 to the squared filter kernel magnitude.
+		freqKernelSqMagnitude += 1 / pow(snr, 2.0);
+	}
+
+	// Split frequency spectrum of degradedPadded image into real and imaginary parts.
+	Mat OApproxChannels[] = {Mat::zeros(sBorder.size(), CV_32FC1), Mat::zeros(sBorder.size(), CV_32F)};
+	split(OApprox, OApproxChannels);
+
+	// Divide each plane by the squared magnitude of the kernel frequency spectrum.
+	// What we have done up to this point: (I * H*) / (Re(H)^2 + Im(H)^2) = I/H
+	divide(OApproxChannels[0], freqKernelSqMagnitude, OApproxChannels[0]); // Re(I) / (Re(H)^2 + Im(H)^2)
+	divide(OApproxChannels[1], freqKernelSqMagnitude, OApproxChannels[1]); // Im(I) / (Re(H)^2 + Im(H)^2)
+
+	// Merge real and imaginary parts of the image frequency spectrum.
+	merge(OApproxChannels, 2, OApprox);
+
+	// Inverse DFT.
+	// Note: DFT_REAL_OUTPUT means that we want the output to be a one-channel matrix again.
+	dft(OApprox, oApprox, DFT_INVERSE | DFT_SCALE | DFT_REAL_OUTPUT);
+
+	// Crop output image to original size.
+	oApprox = oApprox(Rect(bH, bV, oApprox.cols - (bH * 2), oApprox.rows - (bV * 2)));
+
+	return oApprox;
+}
 
 Mat inverseFilter(Mat& degraded, Mat& filter) {
-
-	const float epsilon = 0.09f;
-
-	const Mat& s = degraded;
-	const Mat& p = filter;
-	const int kw = p.cols;
-	const int kh = p.rows;
-
-	Mat pBig = Mat::zeros(s.size(), s.type());
-	for (int x = 0; x < kw; ++x) {
-		for (int y = 0; y < kh; ++y) {
-			pBig.at<float>(x, y) = p.at<float>(x, y);
-		}
-	}
-	Mat pBigShifted(s.size(), s.type());
-	circShift(pBig, pBigShifted, -(kw/2), -(kh/2));
-
-	//	TODO;
-	//	add imaginary part/channel to the pBigShifted matrix,
-	//			to get complex putput from the DFT
-	Mat empty = Mat::zeros(pBigShifted.size(), pBigShifted.type());
-	vector<Mat> pBigShiftedChannels;
-	pBigShiftedChannels.push_back(pBigShifted);
-	pBigShiftedChannels.push_back(empty);
-	Mat pBigShiftedCompl(pBigShifted.size(), CV_32FC2);
-	merge(pBigShiftedChannels, pBigShiftedCompl);
-
-	// transform into frequency domain
-	Mat S(s.size(), CV_32FC2); // complex
-	Mat P(pBigShifted.size(), CV_32FC2); // complex
-//	dft(s, S, DFT_COMPLEX_OUTPUT);
-//	dft(pBigShifted, P, DFT_COMPLEX_OUTPUT);
-//	dft(s, S);
-	dft(s, S, DFT_COMPLEX_OUTPUT);
-//	dft(pBigShifted, P);
-	dft(pBigShiftedCompl, P);
-
-	const int w = P.rows;
-	const int h = P.cols;
-	float PiAbsMax = 0.0f;
-	Mat Pabs(P.size(), CV_32FC1); // abs(P)
-	for (int x = 0; x < w; ++x) {
-		for (int y = 0; y < h; ++y) {
-			Vec2f Pi = P.at<Vec2f>(x, y);
-			float PiAbs = sqrt((Pi[0] * Pi[0]) + (Pi[1] * Pi[1]));
-			if (PiAbs > PiAbsMax) {
-				PiAbsMax = PiAbs;
-			}
-			Pabs.at<float>(x, y) = PiAbs;
-		}
-	}
-
-	// create the inverse filter
-	// see DIP04_ST13_wiener.pdf page 12
-	const float epsMaxAbsP = epsilon * PiAbsMax;
-
-	Mat Preciproce(P.size(), CV_32FC2);
-//	divide(1.0, P, Preciproce); // NOTE this would not do correct complex division
-	for (int x = 0; x < w; ++x) {
-		for (int y = 0; y < h; ++y) {
-			const Vec2f& Pi = P.at<Vec2f>(x, y);
-			const float a2Nb2 = (Pi[0] * Pi[0]) + (Pi[1] * Pi[1]);
-			Preciproce.at<Vec2f>(x, y)[0] =  Pi[0] / a2Nb2;
-			Preciproce.at<Vec2f>(x, y)[1] = -Pi[1] / a2Nb2;
-		}
-	}
-
-	Mat Q(Pabs.size(), CV_32FC2);
-	for (int x = 0; x < w; ++x) {
-		for (int y = 0; y < h; ++y) {
-			const float PiAbs = Pabs.at<float>(x, y);
-			Vec2f Qi(0.0f, 0.0f);
-			if (PiAbs >= epsMaxAbsP) {
-				Qi = Preciproce.at<Vec2f>(x, y);
-			} else {
-				Qi[0] = 1.0f / epsMaxAbsP;
-			}
-			Q.at<Vec2f>(x, y) = Qi;
-		}
-	}
-
-	// multiply with the inverse model of the distortion
-	// (== convolute in spacial domain)
-	Mat O_approx(P.size(), P.type());
-	cout << "S: " << S.size[0] << "*" << S.size[1] << " - " << S.type() << endl;
-	cout << "Q: " << Q.size[0] << "*" << Q.size[1] << " - " << Q.type() << endl;
-	multiply(S, Q, O_approx);
-
-	// transform back into spatial domain
-	Mat o_approx(O_approx.size(), CV_32FC1);
-//	dft(O_approx, o_approx, DFT_INVERSE| DFT_SCALE | DFT_REAL_OUTPUT);
-	dft(O_approx, o_approx, DFT_INVERSE| DFT_SCALE);
-
-	return o_approx;
+	return inverseAndWiener(degraded, filter, -1.0, true);
 }
 
 Mat wienerFilter(Mat& degraded, Mat& filter, double snr) {
-
-	// TODO
-	return degraded;
-}
-
-/*
-Generates a gaussian filter kernel of given size
-kSize:		kernel size (used to calculate standard deviation)
-return:		the generated filter kernel
-*/
-static void createGaussianKernel(Mat& kernel, int kSize) {
-
-//	kernel = Mat::zeros(kSize, kSize, CV_32FC1);
-
-	const int ksh = kSize / 2;
-	const int muX = ksh;
-	const int muY = ksh;
-	const float sigma = kSize / 5.0f;
-	const float sigmaSqr = sigma * sigma;
-	const float gaussMul = 1.0f / (2 * M_PI * sigma * sigma);
-
-	float integral = 0.0f;
-	for (int x = 0; x < kSize; ++x) {
-		for (int y = 0; y < kSize; ++y) {
-			// calculate gaussian coordinates
-			const int gX = x - muX;
-			const int gY = y - muY;
-			// calculate gaussian value
-			const float curVal = gaussMul * exp(-0.5f * ((gX*gX + gY*gY) / sigmaSqr));
-			// store it
-			kernel.at<float>(x, y) = curVal;
-			integral += curVal;
-		}
-	}
-	cout << "Kernel integral (should be ~= 1.0f): " << integral << endl;
-}
-
-void createKernel(Mat& kernel, int kSize, string name) {
-
-	if (name == "gaussian") {
-		createGaussianKernel(kernel, kSize);
-	} else {
-		// TODO
-	}
+	return inverseAndWiener(degraded, filter, snr, false);
 }
 
 void circShift(Mat& in, Mat& out, int dx, int dy) {
